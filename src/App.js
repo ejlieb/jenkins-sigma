bb9094d0955e696e7bd54d60777d5d8f437c0b2f
unknown <coryonronda@gmail.com>
2023-02-23 08:41:03 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change commit
+          test frontend
         </a>
       </header>
     </div>
